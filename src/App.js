063a585a36ec3e861ae8e0c878e8bca3f7a9af20
Titus Muthomi <muthomititus+github@gmail.com>
2023-03-22 10:48:41 +0300

    Add navigation shortcuts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { MdOutlineGroups } from 'react-icons/md';
 import { GrProductHunt } from 'react-icons/gr';
 import { SlOrganization } from 'react-icons/sl';
 import { CgShortcut } from 'react-icons/cg';
+import { AiFillFilePdf, AiFillSetting } from 'react-icons/ai';
 
 function App() {
 	return (
@@ -12,45 +13,46 @@ function App() {
 				<Sidebar.Collapse>
 					<Sidebar.Header>
 						<Sidebar.Brand>
-							<CgShortcut size={70}/>
+							<CgShortcut size={70} />
 						</Sidebar.Brand>
 						<Sidebar.Toggle />
 					</Sidebar.Header>
 					<Sidebar.Body>
-						<Sidebar.Nav>
-							<Sidebar.Nav.Link>
+						<Sidebar.Nav className='d-flex flex-column'>
+							<Sidebar.Nav.Link className='mb-4'>
 								<Sidebar.Nav.Icon>
 									<MdOutlineGroups size={70} />
 								</Sidebar.Nav.Icon>
 								<Sidebar.Nav.Title>Stake Holder Management</Sidebar.Nav.Title>
 							</Sidebar.Nav.Link>
-							<Sidebar.Sub>
-								<Sidebar.Sub.Toggle>
-									<Sidebar.Nav.Icon />
-									<Sidebar.Nav.Title>
-										{/* sub menu item title */}
-									</Sidebar.Nav.Title>
-								</Sidebar.Sub.Toggle>
-								<Sidebar.Sub.Collapse>
-									<Sidebar.Nav>
-										<Sidebar.Nav.Link>
-											<Sidebar.Nav.Icon>
-												{/* sum menu item icon */}
-												<GrProductHunt size={70} />
-											</Sidebar.Nav.Icon>
-                      <Sidebar.Nav.Title>productManagement</Sidebar.Nav.Title>
-										</Sidebar.Nav.Link>
 
-										<Sidebar.Nav.Link> <br />
-											<Sidebar.Nav.Icon>
-												{/* sum menu item icon */}
-												<SlOrganization size={70} />
-											</Sidebar.Nav.Icon>
-                      <Sidebar.Nav.Title>Logistics Management</Sidebar.Nav.Title>
-										</Sidebar.Nav.Link>
-									</Sidebar.Nav>
-								</Sidebar.Sub.Collapse>
-							</Sidebar.Sub>
+							<Sidebar.Nav.Link className='mb-5'>
+								<Sidebar.Nav.Icon>
+									<GrProductHunt size={70} />
+								</Sidebar.Nav.Icon>
+								<Sidebar.Nav.Title>Product Management</Sidebar.Nav.Title>
+							</Sidebar.Nav.Link>
+
+							<Sidebar.Nav.Link className='mb-5'>
+								<Sidebar.Nav.Icon>
+									<AiFillFilePdf size={70} />
+								</Sidebar.Nav.Icon>
+								<Sidebar.Nav.Title>Reporting Management</Sidebar.Nav.Title>
+							</Sidebar.Nav.Link>
+
+							<Sidebar.Nav.Link className='mb-5'>
+								<Sidebar.Nav.Icon>
+									<SlOrganization size={70} />
+								</Sidebar.Nav.Icon>
+								<Sidebar.Nav.Title>Logistics Management</Sidebar.Nav.Title>
+							</Sidebar.Nav.Link>
+
+							<Sidebar.Nav.Link className='mb-5'>
+								<Sidebar.Nav.Icon>
+									<AiFillSetting size={70} />
+								</Sidebar.Nav.Icon>
+								<Sidebar.Nav.Title>Administration</Sidebar.Nav.Title>
+							</Sidebar.Nav.Link>
 						</Sidebar.Nav>
 					</Sidebar.Body>
 				</Sidebar.Collapse>
